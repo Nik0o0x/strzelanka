1b51242d88b698334160072661ca427b1de87406
Niko <nikodem.redmann@wp.pl>
2023-03-20 15:16:17 +0100

    jest progres

--- Assets/SpawnerZombie.cs
+++ Assets/SpawnerZombie.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerZombie: MonoBehaviour
+public class SpawnerZombie : MonoBehaviour
 {
-    public GameObject cubePrefab;
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int EnemyCount;
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (EnemyCount <= 2)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
-            Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
+            xPos = Random.Range(-10, 5);
+            zPos = Random.Range(-10, 5);
+            Instantiate(theEnemy, new Vector3(xPos, 4, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(1);
+            EnemyCount += 1;
         }
     }
 }

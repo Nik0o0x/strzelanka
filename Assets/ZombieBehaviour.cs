941182f4dd0ca38acb872299250f9f9ce40cc562
Niko <nikodem.redmann@wp.pl>
2023-04-03 14:50:23 +0200

    dziala jak ta lala

--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ZombieBehaviour : MonoBehaviour
 {
+    int hp = 3;
     [SerializeField] float health, maxHealth = 3f;
+
     GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -16,17 +18,37 @@ public class ZombieBehaviour : MonoBehaviour
     {
         health -= damageAmount; // 3 -> 2 -> 1 -> 0 = Enemy Has Died
 
-        if(health <= 0) { 
-        Destroy(gameObject);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform.position);
-        //Vector3 playerDirection = transform.position - player.transform.position;
+        if(hp> 0)
+        {
+            transform.LookAt(player.transform.position);
+            //Vector3 playerDirection = transform.position - player.transform.position;
+
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
         
-        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            hp--;
+            if(hp <= 0)
+            {
+                transform.Translate(Vector3.up);
+                transform.Rotate(Vector3.right * -90);
+                GetComponent<BoxCollider>().enabled = false;
+                Destroy(transform.gameObject, 5);
+            }
+        }
     }
 }
